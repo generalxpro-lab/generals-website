50ee328b04f42c7b66b99f12e2efc2ab57721000
generalxpro-lab <sameerxhustler@gmail.com>
2026-08-24 21:27:31 +0530

    Add listing images data structure for new product listings

--- src/data/listing-images.ts
+++ src/data/listing-images.ts
@@ -1,1 +1,14 @@
-export const listingImages = {} as Record<string, string[]>;
+export const listingImages: Record<string, string[]> = {
+  "medicube-zero-pore-pads-2-0-exfoliating-toner-pads-70ct": [
+    "listing-1-image-1",
+    "listing-1-image-2",
+    "listing-1-image-3",
+    "listing-1-image-4",
+  ],
+  "medicube-salmon-dna-pdrn-pink-peptide-serum-glow-firming-1-01-fl-oz": [
+    "listing-2-image-1",
+    "listing-2-image-2",
+    "listing-2-image-3",
+    "listing-2-image-4",
+  ],
+};
